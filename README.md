fa7b67b5608fbf52f97dd31b8d4b3031a4d115b6
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-05-18 08:11:24 -0400

    Successful JSON response from Postgres on localhost.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Testing Postgres database using Sequelize.
+
+Based on [Node API](https://www.robinwieruch.de/postgresql-express-node-rest-api/) tutorials by Robin Wieruch. 
--- server.js
+++ server.js
@@ -1,3 +1,71 @@
 import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+import { Router } from 'express';
 
-console.log('Hello Test Postgres.');
+// Database
+import models, { sequelize } from './models';
+
+// Server
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const eraseDatabaseOnSync = true;
+
+const createUsersWithMessages = async () => {
+  await models.User.create(
+    {
+      username: 'rwieruch',
+      messages: [
+        {
+          text: 'Published the Road to learn React',
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
+
+  await models.User.create(
+    {
+      username: 'ddavids',
+      messages: [
+        {
+          text: 'Happy to release ...',
+        },
+        {
+          text: 'Published a complete ...',
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
+};
+
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  if (eraseDatabaseOnSync) {
+    createUsersWithMessages();
+  }
+
+  app.listen(process.env.PORT, () =>
+    console.log(`!! Postgres app listening on port ${process.env.PORT} !!`),
+  );
+});
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await models.User.findAll();
+    res.json(users);
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500) // If there is any error, tell the client something went wrong on the server
+  }
+})
